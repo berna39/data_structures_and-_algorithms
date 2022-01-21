a0d16304ffeb7df436305b09f174d9f6418bceeb
berna39 <kalemajoseph39@gmail.com>
2022-01-21 21:23:23 +0200

    Evolving with birary search

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,16 @@
 #binary search
+# python language prefers this method alot compared to the recursive method
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        midpoint = (first + last) // 2
+
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+    return None
