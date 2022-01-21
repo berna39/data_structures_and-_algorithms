c809c34730b8d3eb71f0a9b9e0699be1221068bb
berna39 <kalemajoseph39@gmail.com>
2022-01-21 21:25:25 +0200

    Testting not existing value

--- binary_search.py
+++ binary_search.py
@@ -14,3 +14,15 @@ def binary_search(list, target):
         else:
             last = midpoint - 1
     return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in the list")
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = binary_search(numbers, 9)
+verify(result)
